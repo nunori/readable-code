88b923929021473faa9502054f6283b2c985ba0c
이재성 <commonhm@gmail.com>
2025-03-11 23:20:02 +0900

    Refactor: 메서드 분리

---
+++ src/main/java/cleancode/studycafe/tobe/io/tobe/exception/AppException.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.tobe.io.tobe.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+}
